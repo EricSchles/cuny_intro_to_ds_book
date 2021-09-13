b6295982e437e67121f482870a766f5631077401
Eric Schles <ericschles@gmail.com>
2021-09-13 15:20:57 -0400

    first commit

---
+++ _build/jupyter_execute/01/Chapter-1-Introduction.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Chapter 1 Introduction
+# 
+# In this introductory chapter we will provide a whirlwind tour of all the wonderful things you can do with computer science and data science.  There is no expectation that you understand what's going on technically.  Instead, you should use this chapter as an explicit introduction to the material, while seeing how to apply programming and data science to solve problems in science and engineering.
+# 
+# ## Sections
+# 
+# * Basics - 1
+#     * Computation
+#     * Python Programming Language
+#     * Precision In Detail
+#     * Floats
+#     * Trading System Example
+# * Going Deeper on Basics - 2
+#     * Types in Detail 
+#         * ints
+#         * floats
+#         * strings
+#         * booleans
+#         * converting between types
+#         * operators
+#     * imports
+#         * re
+#         * math
+#         * random
+#         * statistics
+#         * datetime
+#         * shutil
+#         * os
+#         * pickle
+#         * json
+#         * time
+#         * sys
+# * Flow of control
+#     * indenting in Python
+#     * if/elif/else
+#     * fizzbuzz
+#     * evens and odds
+# * iteration
+#     * while loops
+#     * lists
+#         * indexing
+#         * slicing
+#         * size
+#     * for loops
+#     
+# start here: https://github.com/EricSchles/datascience_book/blob/master/intro_to_python/engineering_basics/Introduction%20To%20Python.ipynb
+
+# In[ ]:
+
+
+
+
---
+++ intro.md
@@ -0,0 +1,3 @@
+# Welcome
+
+This is the book for the introduction to data science course taught at CUNY in Fall 2021.
